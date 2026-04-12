205c829ac95eb70d9046914570fa0a9826d295d6
Harshita <khushi0812rana@gmail.com>
2026-04-12 21:37:50 +0530

    updated code

---
+++ Q39.java
@@ -0,0 +1,13 @@
+class Q39 {
+public static void main(String[] args) {
+for(int i = 1; i <= 3; i++) {
+for(int s = 1; s < i; s++) {
+System.out.print("  ");
+    }
+for(int j = i; j <= 3; j++) {
+System.out.print("* ");
+    }
+System.out.println();
+}
+    }
+}
