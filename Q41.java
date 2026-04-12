092cae75d054ac00f6ee9b69151f778732fbf2a3
Harshita <khushi0812rana@gmail.com>
2026-04-12 21:43:49 +0530

    updated code

---
+++ Q41.java
@@ -0,0 +1,10 @@
+class Q41 {
+    public static void main(String[] args) {
+    for(int i = 1; i <= 3; i++) {
+    for(int j = i; j >= 1; j--) {
+    System.out.print(j + " ");
+    }
+    System.out.println();
+}
+    }
+}
