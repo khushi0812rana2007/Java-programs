9a713e885e9120f5e3829ecf42d016c5007b8408
Harshita <khushi0812rana@gmail.com>
2026-04-12 19:04:24 +0530

    updated code

---
+++ Q20.java
@@ -0,0 +1,11 @@
+class Q20 {
+    public static void main(String[] args) {
+        int n = 5;
+        int i = 1;
+
+        while(i <= n) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
