dc1521f6677ebac55c8ff07dce7d020c25f4bf5d
Harshita <khushi0812rana@gmail.com>
2026-05-02 11:25:44 +0530

    Added Locker and Main classes

--- locker.java
+++ locker.java
@@ -1,28 +1,27 @@
 class Locker {
-    private int pin = 1234;
-    private boolean isOpen = false;
-    public void checkPin(int enterPin) {
-        if (enterPin == pin) {
-            isOpen = true;
-            System.out.println("Locker is opened");
-        } else {
-            System.out.println("Wrong pin");
-        }
+    private boolean isOpen;
+    private int pin;
+
+    Locker() {
+        isOpen = false;
+        pin = 1234; // default PIN
     }
-    public void checkStatus() {
+
+    void checkStatus() {
         if (isOpen) {
             System.out.println("Locker is open");
         } else {
             System.out.println("Locker is closed");
         }
     }
-}
-class Main {
-    public static void main(String[] args) {
-        Locker A = new Locker();
-        A.checkStatus();
-        A.checkPin(1111);
-        A.checkPin(1234);
-        A.checkStatus();
+
+    void checkPin(int enteredPin) {
+        if (enteredPin == pin) {
+            isOpen = true;
+            System.out.println("Correct PIN! Locker opened.");
+        } else {
+            isOpen = false;
+            System.out.println("Incorrect PIN! Locker remains closed.");
+        }
     }
 }
---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        Locker var1 = new Locker();
+        var1.checkStatus();
+        var1.checkPin(1111);
+        var1.checkPin(1234);
+        var1.checkStatus();
+    }
+}
